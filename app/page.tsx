9df929242f6c248dce686c0d27aa75ebcfd9c935
Vipul Dubey <vipuldubey041@gmail.com>
2026-03-30 08:36:40 +0530

    Changed the landing page text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-black text-white flex justify-center items-center">
-      <div>Landing Page</div>
+      <div>This is the Landing Page</div>
     </div>
   );
 }
